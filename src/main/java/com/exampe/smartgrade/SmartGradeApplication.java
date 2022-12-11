3db8ce751258750f300d7ec8b944fa1defa9b83d
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-10 22:07:18 -0500

    Initial commit

---
+++ src/main/java/com/exampe/smartgrade/SmartGradeApplication.java
@@ -0,0 +1,13 @@
+package com.exampe.smartgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampe/smartgrade/SmartGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampe.smartgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
