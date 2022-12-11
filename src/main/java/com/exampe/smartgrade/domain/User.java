1263d5ad76459a7c6d606db0af7c7708711a8206
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-11 15:56:18 -0500

    constructing JWT layer in process

--- src/main/java/com/exampe/smartgrade/domain/User.java
+++ src/main/java/com/exampe/smartgrade/domain/User.java
@@ -1,18 +1,24 @@
 package com.exampe.smartgrade.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
+    private static final long serialVersionUID = -1605751799209099860L;
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate cohortStartDate;
     private String username;
     private String password;
+//    private List<Authority> authorities = new ArrayList<>();
 
 
     public Long getId() {
@@ -31,14 +37,43 @@ public class User {
         this.cohortStartDate = cohortStartDate;
     }
 
+    @Override
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>() ;
+        roles.add(new Authority("ROLE_STUDENT"));
+        return roles ;
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
@@ -46,5 +81,6 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
 
 }
---
+++ src/main/java/com/exampe/smartgrade/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.exampe.smartgrade.service;
+
+import com.exampe.smartgrade.domain.User;
+import com.exampe.smartgrade.util.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private CustomPasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
+        user.setId(1L);
+        return user;
+    }
+}
