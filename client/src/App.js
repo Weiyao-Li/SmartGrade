a1e559a35b1dfae067a68d006926ef59b4a18e52
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-12 23:38:05 -0500

    react router setup (homepage/dashboard route)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import './App.css';
+import {useEffect} from "react";
+import {useLocalState} from "./util/useLocalStorage";
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Homepage from "./Homepage";
+
 
 function App() {
-    console.log('hello')
-    const reqBody = {
-        "username": "weiyao",
-        "password": "asdfasdf"
-    }
-    fetch("api/auth/login", {
-        headers: {"Content-Type": "application/json"},
-        method:"post",
-        body: JSON.stringify(reqBody)}).then(response=> Promise.all([response.json(), response.headers]))
-        .then(([body, headers]) => {
-            const authValue = headers.get("authorization");
-            console.log(authValue)
-            console.log(body)
-        })
+    const [jwt, setJwt] = useLocalState("", "jwt");
 
-  return (
-    <div className="App">
-        <h1>Hello World!</h1>
-    </div>
-  );
+    useEffect(() => {
+        if (!jwt) {
+            const reqBody = {
+                "username": "weiyao",
+                "password": "asdfasdf"
+            }
+            fetch("api/auth/login", {
+                headers: {"Content-Type": "application/json"},
+                method: "post",
+                body: JSON.stringify(reqBody)
+            }).then(response => Promise.all([response.json(), response.headers]))
+                .then(([body, headers]) => {
+                    setJwt(headers.get("authorization"));
+
+                });
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(`JWT is : ${jwt}`);
+    }, [jwt])
+
+    return (
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/" element={<Homepage/>}/>
+        </Routes>
+    );
 }
 
 export default App;
