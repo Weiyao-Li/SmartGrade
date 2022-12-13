7e3b87b04e8d0340b2ae6e4c3b72fc1519c137d7
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-12 22:13:27 -0500

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+    console.log('hello')
+    const reqBody = {
+        "username": "weiyao",
+        "password": "asdfasdf"
+    }
+    fetch("api/auth/login", {
+        headers: {"Content-Type": "application/json"},
+        method:"post",
+        body: JSON.stringify(reqBody)}).then(response=> Promise.all([response.json(), response.headers]))
+        .then(([body, headers]) => {
+            const authValue = headers.get("authorization");
+            console.log(authValue)
+            console.log(body)
+        })
+
+  return (
+    <div className="App">
+        <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
