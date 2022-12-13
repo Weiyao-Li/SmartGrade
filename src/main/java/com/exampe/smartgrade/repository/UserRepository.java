a538f47830068fce6c6c4f1b8272fce406077f3b
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-12 21:13:06 -0500

    jwt setup

---
+++ src/main/java/com/exampe/smartgrade/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.exampe.smartgrade.repository;
+
+import com.exampe.smartgrade.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
--- src/main/java/com/exampe/smartgrade/service/UserDetailsServiceImpl.java
+++ src/main/java/com/exampe/smartgrade/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.exampe.smartgrade.service;
 
 import com.exampe.smartgrade.domain.User;
+import com.exampe.smartgrade.repository.UserRepository;
 import com.exampe.smartgrade.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,18 +9,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private CustomPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
-        user.setId(1L);
-        return user;
+
+        Optional<User> userOpt = userRepo.findByUsername(username);
+        return userOpt.orElseThrow(()-> new UsernameNotFoundException("Invalid credentials"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
