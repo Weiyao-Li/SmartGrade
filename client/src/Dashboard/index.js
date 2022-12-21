d6388cbd26785b4e1a20606266c905e50fce0683
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2022-12-20 22:28:00 -0500

    css autolayout

--- client/src/Dashboard/index.js
+++ client/src/Dashboard/index.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {useLocalState} from "../util/useLocalStorage";
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import ajax from "../Services/fetchService";
+import {Button, Card} from "react-bootstrap";
 
 const Dashboard = () => {
     const [jwt, setJwt] = useLocalState("", "jwt");
@@ -11,7 +12,7 @@ const Dashboard = () => {
         ajax("api/assignments", "GET", jwt).then(assignmentsData => {
             setAssignments(assignmentsData);
         })
-    }, []);
+    }, [jwt]);
 
 
     function createAssignment() {
@@ -22,10 +23,32 @@ const Dashboard = () => {
 
     return (
         <div style={{marginTop: "2em"}}>
-            {assignments ? assignments.map(assignment =>
-                <div key={assignment.id}><Link to={`/assignments/${assignment.id}`}>Assignment
-                    ID: {assignment.id} </Link>
-                </div>) : (<></>)}
+            {assignments ? (
+                <div className="d-grid gap-5" style={{gridTemplateColumns: "repeat(auto-fit, 18rem)"}}>
+                    {assignments.map(assignment => (
+                        <Card key={assignment.id}
+                              style={{width: '18rem', height: '18rem'}}>
+                            <Card.Body className="d-flex flex-column justify-content-around">
+                                <Card.Title>Assignment #{assignment.id}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{assignment.status}</Card.Subtitle>
+                                <Card.Text style={{marginTop: '1em'}}>
+                                    <p><b>Github URL:</b> {assignment.githubUrl}</p>
+                                    <p><b>Branch: </b>{assignment.branch}</p>
+                                </Card.Text>
+
+                                <Button onClick={() => {
+                                    window.location.href = `/assignments/${assignment.id}`;
+                                }}
+                                >Edit</Button>
+
+                            </Card.Body>
+                        </Card>
+
+                    ))}
+                </div>
+            ) : (
+                <></>
+            )}
             <button onClick={() => createAssignment()}>Submit New Assignment</button>
         </div>
     );
